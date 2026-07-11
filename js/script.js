a5c9742a039660bc5ec4d3f83b62c9bc92953601
14601-rgb <14601@phanthong.ac.th>
2026-07-11 12:35:38 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
+document.addEventListener("DOMContentLoaded",()=>{
 
+const cards=document.querySelectorAll(".card");
+
+cards.forEach((card,index)=>{
+
+card.style.opacity="0";
+
+setTimeout(()=>{
+card.style.transition=".8s";
+card.style.opacity="1";
+card.style.transform="translateY(0)";
+},index*250);
+
+});
+
+});
